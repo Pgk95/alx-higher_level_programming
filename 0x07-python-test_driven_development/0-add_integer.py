3b3ec0c8cbc9bbc868379bc73a8688f50fdfbc11
pgk95 <kalupgk@gmail.com>
2023-04-07 05:01:52 -0700

    quick fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-# 0-add_integer.py
-"""Defines an integer addition function."""
 
-
-def add_integer(a, b=98):   
+def add_integer(a, b=98):
     """Return the integer addition of a and b.
     float arguments are tyoecasted to ints before addition is performed.
     Raises:
