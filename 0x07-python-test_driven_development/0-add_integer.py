f03f3ddc14f4b5ec0235d11e20a6868740def1ec
pgk95 <kalupgk@gmail.com>
2023-04-07 04:23:37 -0700

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer a must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+this directory is for the project python test driven development.
