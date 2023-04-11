74e821dff7aba8172ee8a9e25d61cf05075c99d3
pgk95 <kalupgk@gmail.com>
2023-04-11 02:26:46 -0700

    quick fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 
-"""this module is to read a text file."""
-
-# Define a function to read a text file and print its contents to stdout
-
-
+# This function reads a text file (UTF-8) and prints its contents to stdout.
+# It uses the 'with' statement to ensure proper file handling and doesn't handle
+# any file permission or file not found exceptions.
 def read_file(filename=""):
-
+    # Open the file in read mode with UTF-8 encoding
     with open(filename, "r", encoding="utf-8") as file:
-
+        # Read the contents of the file into a string
         contents = file.read()
-        # Read file contents
+        # Print the contents to stdout without adding a newline at the end
         print(contents, end="")
-        # Print file contents to stdout
