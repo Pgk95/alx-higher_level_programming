28c6f5bd6627855bf3118d05dd2c42509cad4e40
Pgk95 <117736725+Pgk95@users.noreply.github.com>
2023-04-11 15:27:12 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-# This function reads a text file (UTF-8) and prints its contents to stdout.
-# It uses the 'with' statement to ensure proper file handling and doesn't handle
-# any file permission or file not found exceptions.
+"""This function reads a text file (UTF-8) and prints its contents to stdout"""
+
 def read_file(filename=""):
     # Open the file in read mode with UTF-8 encoding
     with open(filename, "r", encoding="utf-8") as file:
