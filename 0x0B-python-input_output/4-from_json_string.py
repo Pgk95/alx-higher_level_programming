f090ac5c538f49a4071c19d94b0c4c1cd9ae073c
pgk95 <kalupgk@gmail.com>
2023-04-12 03:52:49 -0700

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pytho3
+
+"""Return a Python data structure represented by a JSON string"""
+
+import json
+
+
+def from_json_string(my_str):
+    """Return a Python data structure represented by a JSON string"""
+    return json.loads(my_str)
