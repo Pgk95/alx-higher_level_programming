ed3c6bc90ecf5cd403887ca0d83ae0efe997e0e2
pgk95 <kalupgk@gmail.com>
2023-04-12 04:22:35 -0700

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+class Student:
+    """Represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student with their first name, last name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Return the dictionary representation of a Student instance"""
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age,
+        }
