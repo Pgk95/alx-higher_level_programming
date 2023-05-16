a9c499bccb37ce5cf89cb2baf6d682542a70424d
pgk95 <kalupgk@gmail.com>
2023-05-16 09:11:52 -0700

    quick fix on 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,9 +1,3 @@
 -- Inserts a new row in the table
 
-INSERT INTO hbtn_0c_0.first_table (id, name)
-VALUES (89, 'Best School');
-
--- Displays the inserted row
-SELECT id, name
-FROM hbtn_0c_0.first_table
-WHERE ID = 89;
+INSERT INTO 'first_table' (id, name) VALUES (89, 'Best School');
