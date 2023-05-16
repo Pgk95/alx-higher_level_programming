0aaebf7f7d5041737ddc1f7cc757398b6a8986fd
pgk95 <kalupgk@gmail.com>
2023-05-16 09:04:54 -0700

    quick fix task5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
--- Prints the full description of the table first_table
-SELECT CONCAT('Table	', TABLE_NAME, '     ', CREATE_TABLE)
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-	AND TABLE_NAME = 'first_table';
+-- displays the full description of the table first_table from specified database.
+SHOW CREATE TABLE 'first_table';
