42ff70d94fc321e4d37cab3620a028c86d5a7d28
pgk95 <kalupgk@gmail.com>
2023-05-18 03:53:51 -0700

    quick fix on task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,7 @@
 -- This script lists al componenets that can be found in a database.
-USE hbtn_0d_usa;
-
-SELECT cities.id, cities.name
-FROM cities, states
-WHERE cities.state_id = (
+SELECT id, name
+FROM cities
+WHERE state_id = (
 	SELECT id
 	FROM states
 	WHERE name = 'California'
