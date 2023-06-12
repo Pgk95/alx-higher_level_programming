95c617f190e911457a479ce6512ec8a44baa438a
Pgk95 <117736725+Pgk95@users.noreply.github.com>
2023-06-12 15:31:51 +0300

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,15 @@
 
 const argsCount = process.argv.length - 2;
 
-if (argsCount === 0) {
+if (argsCount === 0)
+{
 	console.log('No argument');
-} else if (argsCount === 1) {
+}
+else if (argsCount === 1)
+{
 	console.log('Argument found');
-} else {
+}
+else
+{
 	console.log('Arguments found');
 }
