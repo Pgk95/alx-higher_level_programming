f221bd477141f00740d1879ae06d334b069e9247
pgk95 <kalupgk@gmail.com>
2023-06-12 07:36:16 -0700

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n)) {
+    return (1);
+  }
+
+  if (n === 0 || n === 1) {
+    return (1);
+  }
+
+  return (n * factorial(n - 1));
+}
+
+const argument = parseInt(process.argv[2]);
+
+const result = factorial(argument);
+console.log(result);
