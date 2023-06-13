d7dd1549f5a5adca7c76ebd68573d11cce072cc8
pgk95 <kalupgk@gmail.com>
2023-06-13 05:49:28 -0700

    quick fix

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return {};
-    }
-
-    this.width = w;
-    this.height = h;
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 }
 
