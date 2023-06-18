6ba36135e62ae7dd0cd03d0745855eebc9173de2
pgk95 <kalupgk@gmail.com>
2023-06-18 05:53:52 -0700

    task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+
+def list_states(username, password, database):
+    connection = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+    )
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+
+    cursor.close()
+    connection.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+This directory is for the project for python object relational mapping.
