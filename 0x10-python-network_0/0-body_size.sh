66d53756c207df6383bd439d94658d72ee9f9571
pgk95 <kalupgk@gmail.com>
2023-06-30 05:10:29 -0700

    quick fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # this script takes in a URL and displays the body by sending a GET request.
-[ -z "$1" ] && { echo "Usage: ./0-body_size.sh <URL>"; exit 1; } ; curl -s "$1" | awk '/^$/{p=0} p; /^c\Content-Length:/{p=1}'
+curl-s "$1" | wc -c
