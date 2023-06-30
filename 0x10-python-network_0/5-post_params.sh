b3d6a055276c8b5ec91f35cc91ff7b52c04f6753
Pgk95 <117736725+Pgk95@users.noreply.github.com>
2023-06-30 18:35:02 +0300

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes URL and sends POST request and display the body of the response.
-cur -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
