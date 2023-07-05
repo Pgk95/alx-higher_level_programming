4e32bcb92043d3983ec3964f69f396ff5cd226df
pgk95 <kalupgk@gmail.com>
2023-07-05 01:34:32 -0700

    task 3

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import urllib.request
+import urllib.parse
+import sys
+
+
+def send_post_request(url, email):
+    """
+    sends a POST request to a given URL with the email.
+    """
+
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    with urllib.request.urlopen(url, data=data) as response:
+        body = response.read().decode('utf-8')
+
+    print(body)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        sys.exit(1)
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    send_post_request(url, email)
