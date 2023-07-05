d6c2ee501e19b9d442f3f4de11358d10499abd14
pgk95 <kalupgk@gmail.com>
2023-07-05 01:20:45 -0700

    task fixed

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,21 +5,17 @@
 import urllib.request
 import sys
 
-<<<<<<< HEAD
+
 def main():
     url = sys.argv[1]
-=======
->>>>>>> c665bc17f91f5542471a5c96622ad2d265da4964
+
 
 def main():
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
         x_request_id = response.headers.get('X-Request-Id')
     print(x_request_id)
+
+
 if __name__ == "__main__":
-<<<<<<< HEAD
-=======
-    
-    
->>>>>>> c665bc17f91f5542471a5c96622ad2d265da4964
     main()
