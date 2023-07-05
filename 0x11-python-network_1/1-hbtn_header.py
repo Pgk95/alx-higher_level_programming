ec807ea58b3d30021be1795736847d30d07703ac
pgk95 <kalupgk@gmail.com>
2023-07-05 00:33:54 -0700

    quick fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""this script takes in a URL sends a request and displays the header of the response"""
+"""this script takes in a URL sends a
+request,get the header of the response"""
 
 import urllib.request
 import sys
