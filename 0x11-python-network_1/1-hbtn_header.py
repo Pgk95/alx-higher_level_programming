acdd4cf3204fad048039a405692c08eadaaae97e
pgk95 <kalupgk@gmail.com>
2023-07-05 00:59:38 -0700

    quick fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,9 +5,13 @@
 import urllib.request
 import sys
 
-if __name__ == "__main__":
+def main():
     url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
-        x_request_id = response.headers.get('X-request-Id')
-print(x_request_id)
+        x_request_id = response.headers.get('X-Request-Id')
+
+    print(x_request_id)
+
+if __name__ == "__main__":
+    main()
